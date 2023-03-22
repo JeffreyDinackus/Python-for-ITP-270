f552a663629d8ec0eea4c63a24a65f0afef1b39e
JeffreyDinackus <z3l9m@protonmail.com>
2023-03-21 20:36:26 -0400

    updated

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,9 @@
 #!/bin/python3
 
 
-print("Hello World!")
+#I think these descriptions are fun
+user_description = "Pie Lover"
+print("Hello World! I know you are a " + user_description + "!") 
 
-
+user_description = "Dirty Dancer"
+print("Hello World! I know you are a " + user_description + "!") 
